8e86ee18853179566818c113ce6e57fa56c18e9e
Jesper Turesson <Jturesson90@gmail.com>
2021-12-07 10:30:32 +0100

    Persistence between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,21 +11,23 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text BestScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+    private GameController controller;
     // Start is called before the first frame update
     void Start()
     {
+        controller = FindObjectOfType<GameController>();
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -35,6 +37,10 @@ public class MainManager : MonoBehaviour
                 brick.PointValue = pointCountArray[i];
                 brick.onDestroyed.AddListener(AddPoint);
             }
+        }
+        if (BestScoreText && controller && controller.BestPlayer != null)
+        {
+            BestScoreText.text = $"Best Score : {controller.BestPlayer.playerName} : {controller.BestPlayer.score}";
         }
     }
 
@@ -72,5 +78,10 @@ public class MainManager : MonoBehaviour
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+        controller.SubmitScore(controller.PlayerName, m_Points);
+        if (BestScoreText && controller && controller.BestPlayer != null)
+        {
+            BestScoreText.text = $"Best Score : {controller.BestPlayer.playerName} : {controller.BestPlayer.score}";
+        }
     }
 }
